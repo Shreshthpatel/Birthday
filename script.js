ed125d36220fff09ec3ee1f02cf787aee775436c
Shreshthpatel <71976512+Shreshthpatel@users.noreply.github.com>
2025-03-05 13:01:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 document.getElementById('start-button').addEventListener('click', function() {
+    // Hide the start screen and show the main screen
     document.getElementById('start-screen').classList.add('hidden');
     document.getElementById('main-screen').classList.remove('hidden');
 
-    // Start background music
-    const bgMusic = document.getElementById('bg-music');
-    bgMusic.play();
-
-    // Fade in the
+   
